1094daa8d4cce93b93f9490baf1787df9ad8000b
fajfhasjhs <juniorfais21@gmail.com>
2024-03-13 18:26:38 +0700

    Mengubah warna surface cyan menjadi green

--- app/src/main/java/com/example/myapplicationjjtgyhjgjgthjgjgjgjh/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationjjtgyhjgjgthjgjgjgjh/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.myapplicationjjtgyhjgjgthjgjgjgjh
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -37,7 +36,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String) {
     Surface(color= Color.Green) {
         Text(
             text = "Hello $name! Kamu pasti bisa menyelesaikan semua!",
